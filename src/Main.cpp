7148196cf998cf130dc16fcb6751a749b6ab4af5
HaDriell <gautierlathu@hotmail.fr>
2023-08-07 14:15:14 +0200

    Initial Commit

---
+++ src/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "nlohmann/json.hpp"
+using json = nlohmann::json;
+
+void GetDeckInfo();
+
+int main()
+{
+    json object;
+    std::cout << "Hello World" << std::endl;
+}
