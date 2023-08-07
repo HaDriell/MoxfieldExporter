2b5f5d20af9e3f54702a51a86996f47e287a467f
HaDriell <gautierlathu@hotmail.fr>
2023-08-07 15:16:47 +0200

    Moving to LibCPR

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 
+#include "cpr/cpr.h"
 #include "nlohmann/json.hpp"
 using json = nlohmann::json;
 
-void GetDeckInfo();
+json FetchDeckFromMoxfield(const std::string& moxfieldDeckID)
+{
+    std::string url = "https://api2.moxfield.com/v2/decks/all/" + moxfieldDeckID;
+    cpr::Response response = cpr::Get(cpr::Url(url));
+    return json(response.text);
+}
 
 int main()
 {
-    json object;
-    std::cout << "Hello World" << std::endl;
+    json deckInfo = FetchDeckFromMoxfield("JmfIh2Z5MEK2DlK6WfdBww");
+    std::cout << "Hello World" << deckInfo << std::endl;
 }
